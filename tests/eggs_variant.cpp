fdeacd3a33175f6a889ead2502b641e4fd0e25ac
TyRoXx <tyroxxxx@gmail.com>
2016-02-23 18:54:58 +0100

    set up project structure

---
+++ tests/eggs_variant.cpp
@@ -0,0 +1,6 @@
+#include <eggs/variant.hpp>
+#include "catch.hpp"
+
+TEST_CASE("eggs-variant", "visit")
+{
+}
---
+++ tests/main.cpp
@@ -0,0 +1,3 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <silicium/terminate_on_exception.hpp>
